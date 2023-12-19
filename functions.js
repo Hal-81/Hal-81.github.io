eeb8e19a76343dadbd2a271bc36cd1865d4bfed2
Hal-81 <valerianpratama06@gmail.com>
2023-12-19 17:27:49 +0700

    modify

--- functions.js
+++ functions.js
@@ -13,6 +13,7 @@ function lower() {
 function showNumber() {
     document.getElementById("number").innerHTML = number;
 }
+// ___________________________________________________________________________________________________________________
 
 document.addEventListener('DOMContentLoaded', function () {
     // Show the pop-up when the page loads
@@ -28,3 +29,25 @@ function closePopup() {
     var popupContainer = document.getElementById('popup-container');
     popupContainer.style.display = 'none';
 }
+// ___________________________________________________________________________________________________________________
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Get references to the button and menu
+    var toggleButton = document.getElementById('toggleButton');
+    var dividers = document.getElementById('navbar');
+    var reopen = document.getElementById('reopen');
+
+    // Toggle menu visibility on button click
+    toggleButton.addEventListener('click', function() {
+        dividers.classList.toggle('closed');
+        reopen.style.display = 'block';
+    });
+
+    // Reopen menu on click
+    reopen.addEventListener('click', function() {
+        dividers.classList.remove('closed');
+        setTimeout(function() {
+            reopen.style.display = 'none';
+        }, 600)
+    });
+});
