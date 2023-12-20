7a2625fe6caa225e0621f0ff099873c350fda5ca
Hal-81 <valerianpratama06@gmail.com>
2023-12-20 18:01:44 +0700

    modify

--- functions.js
+++ functions.js
@@ -15,11 +15,6 @@ function showNumber() {
 }
 // ___________________________________________________________________________________________________________________
 
-document.addEventListener('DOMContentLoaded', function () {
-    // Show the pop-up when the page loads
-    showPopup();
-});
-
 function showPopup() {
     var popupContainer = document.getElementById('popup-container');
     popupContainer.style.display = 'flex';
@@ -51,3 +46,58 @@ document.addEventListener('DOMContentLoaded', function() {
         }, 600)
     });
 });
+// ___________________________________________________________________________________________________________________
+
+var totalPrice = 0;
+function calculateTotalAndRedirect() {
+    var button = event.target;
+    var individualPrice = button.getAttribute('data-price');
+
+
+    totalPrice += parseFloat(individualPrice);
+    document.getElementById("totalAmount").innerHTML = "total: Rp" + totalPrice;
+
+        // Save the updated total to sessionStorage
+        sessionStorage.setItem('totalAmount', totalPrice);
+
+        // Redirect to the payment page
+}
+
+function showCart() {
+    var button = event.target;
+    var price = button.getAttribute('data-price');
+    var dataName = button.getAttribute('data-name');
+
+    var paragraph = document.createElement('p');
+
+    // Set the content of the paragraph
+    paragraph.textContent = dataName + ' Rp' + price;
+    paragraph.style.fontSize = '0.6em';
+    paragraph.style.margin = '1px';
+
+    // Append the paragraph to the cartPopup element
+    document.getElementById("cartPopup").appendChild(paragraph);
+}
+
+function clearCart() {
+    document.getElementById("totalAmount").innerHTML = "Total: 0"
+    document.getElementById("cartPopup").innerHTML = ''
+}
+
+function showSuccessAlert() {
+    // Create an alert element
+    var alertElement = document.createElement('div');
+    alertElement.className = 'alert alert-success position-fixed top-0 end-0';
+    alertElement.setAttribute('role', 'alert');
+    alertElement.innerHTML = 'Item bought successfully!';
+  
+    // Append the alert to the body
+    document.body.appendChild(alertElement);
+  
+    // Set a timeout to remove the alert after 2 seconds
+    setTimeout(function() {
+      alertElement.remove();
+    }, 2000);
+  }
+  
+
